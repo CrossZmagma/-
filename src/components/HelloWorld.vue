07b382839fe229544c1f96d903d70070026fbccc
CrossZmagma <1149429193@qq.com>
2019-07-25 17:32:32 +0800

    修改了布局和表达形式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,21 +6,42 @@
       用户指引
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">超链接1</a>.
     </p>
-    <h3>您的权益受到了侵害？</h3>
     <ul>
+    <h3>您需要联系快递公司？</h3>
+    </ul>
+      <p>
+        <a href="http://www.zto.com">
+          <img border="0" src="./../../public/img/1.jpg" />
+        </a>
+      </p>
+      <p>
+        <a href="http://www.sto.cn">
+          <img border="0" src="./../../public/img/2.jpg" />
+        </a>
+      </p>
+      <p>
+        <a href="http://800bestex.com">
+          <img border="0" src="./../../public/img/3.jpg" />
+        </a>
+      </p>
+      <p>
+        <a href="http://http://www.sf-express.com/cn/sc/">
+          <img border="0" src="./../../public/img/4.jpg" />
+        </a>
+      </p>
+      <p>
+        <a href="http://http://www.deppon.com/newwebsite">
+          <img border="0" src="./../../public/img/5.jpg" />
+        </a>
+      </p>
+    <ul>
+    <h3>您的权益受到了侵害？</h3>
+    </ul>
       <li><a href="http://www.315.gov.cn/" target="_blank" rel="noopener">中国消费者权益保护网</a></li>
       <li><a href="https://jubao.taobao.com/t" target="_blank" rel="noopener">淘宝网举报中心</a></li>
       <li><a href="https://myjd.jd.com/afs/indexNew.action?entry=2" target="_blank" rel="noopener">京东售后服务</a></li>
       <li><a href="https://help.suning.com/page/id-469.htm" target="_blank" rel="noopener">苏宁联系客服</a></li>
-    </ul>
-    <h3>您需要联系快递公司？</h3>
-    <ul>
-      <li><a href="http://www.zto.com" target="_blank" rel="noopener">中通快递</a></li>
-      <li><a href="http://www.sto.cn" target="_blank" rel="noopener">申通快递</a></li>
-      <li><a href="http://800bestex.com" target="_blank" rel="noopener">百世快递</a></li>
-      <li><a href="http://www.sf-express.com/cn/sc/" target="_blank" rel="noopener">顺丰快递</a></li>
-      <li><a href="http://www.deppon.com/newwebsite" target="_blank" rel="noopener">德邦快递</a></li>
-    </ul>
+
     <h3>有什么可以帮到您？</h3>
     <ul>
       <li><a href="http://www.baidu。com/" target="_blank" rel="noopener">个人账户</a></li>
